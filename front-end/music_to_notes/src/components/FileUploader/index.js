9ced88c928c91bc8e535cefba0fcfb91e28364d4
asif-mammadov <asif.mammad@hotmail.com>
2022-03-30 10:03:50 +0400

    added pdf download support on the front-end

--- front-end/music_to_notes/src/components/FileUploader/index.js
+++ front-end/music_to_notes/src/components/FileUploader/index.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+import download from 'downloadjs'
 
 export const FileUploader =  ({})=>{
     const [file,setFile] = useState(null);
@@ -14,9 +15,11 @@ export const FileUploader =  ({})=>{
         e.preventDefault();
         const data = new FormData();
         data.append('file', file );
+        data.append('type', 'pdf')
         axios.post('http://localhost:5000/api/audio',data)
-            .then((e)=>{
+            .then((response)=>{
                 console.log("Successfuly sent the file to server")
+                download(response.data, "stylesheet", response.headers['content-type'])
             })
             .catch((e)=>{
                 console.log('Error',e)
