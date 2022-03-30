57d207e01a52f1ba1cdeed0b36b8696c5433056a
asif-mammadov <asif.mammad@hotmail.com>
2022-03-30 09:25:35 +0400

    added png support

--- flask-api/controllers/apiController.py
+++ flask-api/controllers/apiController.py
@@ -3,7 +3,7 @@ import os
 import datetime as dt
 
 from services.NoteExtractor import NoteExtractor
-from services.converter import convert_to_pdf
+from services.converter import *
 from services.print_sheet_music import print_sheet_music
 
 DIR_PATH = "./uploaded_files/"
@@ -16,14 +16,17 @@ def getDateTime():
   return jsonify(datetime=now)
 
 def postAudio():
-  # if (request.files['file'].content_type[:5] != "audio"):
-  #   return Response("Unsupported file type", status=400)
+  if (request.files['file'].content_type[:5] != "audio"):
+    return Response("Unsupported file type", status=400)
   file_path = os.path.join(DIR_PATH, request.files['file'].filename)
   request.files['file'].save(file_path)
+  file_type = request.form['type']
+  if file_type not in ['pdf', 'png']:
+    return Response("Unsupported file type", status=400)
   ne = NoteExtractor(file_path)
   out = ne.get_all_estimated_freqs()
   print("Freq and length: ", out)
   freqs = [a[0] for a in out]
   print_sheet_music(file_path, freqs)
-  pdf_path = convert_to_pdf(file_path)
+  pdf_path = convert(file_path, file_type)
   return send_file(pdf_path)
--- flask-api/services/converter.py
+++ flask-api/services/converter.py
@@ -1,10 +1,10 @@
 import os
 import shutil
 
-def convert_to_pdf(file_path:str):
-  stream = os.system("lilypond" + " " + file_path)
+def convert(file_path:str, file_type:str):
+  stream = os.system("lilypond" + " --" + file_type + " " + file_path)
   filename = file_path.split("/")[-1].strip()
   path = file_path[:-len(filename)]
-  filename = filename.split(".")[0] + ".pdf"
+  filename = filename.split(".")[0] + "." + file_type
   shutil.move(filename, path + filename)
   return path + filename
