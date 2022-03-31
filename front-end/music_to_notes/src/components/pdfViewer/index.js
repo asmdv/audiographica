94b9e0f056b72b02388559f5fe9f9b75173a86ee
asif-mammadov <asif.mammad@hotmail.com>
2022-03-31 10:57:37 +0400

    fixed pdf export bug

--- front-end/music_to_notes/src/components/pdfViewer/index.js
+++ front-end/music_to_notes/src/components/pdfViewer/index.js
@@ -1,42 +1,44 @@
-import React, {useEffect} from "react";
-import { useState } from "react";
-import image from './Notes.png'
+import React from "react";
 import './pdfViewer.css'
 import axios from "axios";
-import download from "downloadjs";
-  
+import FileSaver from 'file-saver';
 
-const ViewPdf = ({ image })=>{
+
+const ViewPdf = ({image}) => {
 
     const extract_name = (name) => {
         return name.split(".")[0]
     }
 
-    const onSubmit = (e) =>{
+    const onSubmit = (e) => {
         e.preventDefault();
 
         var filename = localStorage.getItem("filename")
         var origName = localStorage.getItem("origName")
-        axios.post('http://localhost:5000/api/pdf', {filename: filename})
-            .then((response)=>{
+        console.log("Filename is ", filename)
+        return axios.post('http://localhost:5000/api/pdf', {filename: filename}, {
+            responseType: 'blob'
+        })
+            .then((response) => {
                 console.log("Successfuly sent the file to server")
-                console.log("Response", response)
-                download(response.data, extract_name(origName) + ".pdf", response.headers['content-type'])
+                var blob = new Blob([response.data], {type: "application/pdf"})
+                FileSaver.saveAs(blob, extract_name(origName) + ".pdf")
             })
-            .catch((e)=>{
-                console.log('Error',e)
+            .catch((e) => {
+                console.log('Error', e)
 
             })
 
 
     };
 
-    return ( 
-    <div id="display">
-        <a class="ui inverted green button" id="download" href={image} download onClick={onSubmit}><i class="download icon"></i>Click to download </a>
-        <button class="ui inverted red button" > Clear </button>
-        <img src={image} alt="Sheet Music failed to upload" />
-    </div>);
-}; 
+    return (
+        <div id="display">
+            <a class="ui inverted green button" id="download" href={image} download onClick={onSubmit}><i
+                class="download icon"></i>Click to download </a>
+            <button class="ui inverted red button"> Clear</button>
+            <img src={image} alt="Sheet Music failed to upload"/>
+        </div>);
+};
 
 export default ViewPdf;
